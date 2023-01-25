8fc65f30a3dfcc7e033084b8806d26ea238e4174
abrarasif11 <faasif929@gmail.com>
2023-01-25 21:56:28 +0600

    added-header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Main from './pages/Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+   {
+    path: '/',
+    element : <Main></Main> ,
+    children : [
+      {
+       path : '/',
+       element : <Home></Home> ,
+      },
+
+    ]
+   }
+  ])
   return (
     <div className="App">
-     
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
